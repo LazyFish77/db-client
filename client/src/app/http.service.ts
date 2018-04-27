41a1f4372ac7ec30f28620e8afc4c8afdfab54c9
Tyler Fischer <fischt77@uwosh.edu>
2018-04-27 00:56:32 -0500

    login form update

--- client/src/app/http.service.ts
+++ client/src/app/http.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class HttpService {
 
   constructor(private http: HttpClient) { }
+
+  public login(username, password): Observable<any> {
+      let formData = new FormData()
+      formData.append('username', username);
+      formData.append('password', password);
+      formData.append('loginUser', 'true');
+      return this.http.post('http://webdev.cs.uwosh.edu/students/fischt77/login.php', formData);
+  } 
+  public registerUser(username, password): Observable<any> {
+    let formData = new FormData()
+    formData.append('username', username);
+    formData.append('password', password);
+    formData.append('registerUser', 'true');
+    return this.http.post('http://webdev.cs.uwosh.edu/students/fischt77/login.php', formData);
+  }
+
 
 }
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, } from '@angular/forms';
 import { HttpService } from '../http.service';
 @Component({
   selector: 'app-login',
@@ -7,12 +7,29 @@ import { HttpService } from '../http.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public username: string;
+  public password: string;
+  constructor(private httpSrv: HttpService) { }
 
   ngOnInit() {
+    
   }
   public login(): void {
+    this.httpSrv.login(this.username, this.password)
+      .subscribe(
+        x => console.log(x),
+        e => console.log(e));
+  }
+  public registeruser(): void {
+    this.httpSrv.registerUser(this.username, this.password)
+      .subscribe(
+        x => console.log(x),
+        e => console.log(e));
+  }
+
+  public clear() : void {
+    this.username = null;
+    this.password = null;
   }
 
 }
