aca5cbd607a82d7d31e56dbf3018fc64ee827873
Tyler Fischer <fischt77@uwosh.edu>
2018-04-26 22:30:19 -0500

    making some componers

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
-
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
